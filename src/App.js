08bd8dd4758d85d491a7c32d44d97a6f3846d9fe
Manya Khullar <manyakhullar457@gmail.com>
2024-10-11 19:26:29 -0400

    Set up GitHub Actions for deployment

--- src/App.js
+++ src/App.js
@@ -13,7 +13,8 @@ const App = () => {
   return (
     <div className="App">
       <h1>Student Information System</h1>
-      <h5>Developed By: 101410731 || Manya Khullar || DevOps</h5>
+      <h5>Developed By: 101410731 || Manya Khullar
+         || DevOps</h5>
       <StudentForm addStudent={addStudent} />
       <StudentList students={students} />
     </div>
